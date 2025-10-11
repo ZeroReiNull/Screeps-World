b662b27fba6325e65532190b35e35c4612fa7d73
ZeroTsu <103555203+ZeroReiNull@users.noreply.github.com>
2025-10-11 00:10:03 -0400

    Add stucture tower to harvester delivery list

--- role.harvester.js
+++ role.harvester.js
@@ -14,7 +14,8 @@ const roleHarvester = {
             const target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                 filter: (structure) => {
                     return (structure.structureType === STRUCTURE_EXTENSION ||
-                            structure.structureType === STRUCTURE_SPAWN) &&
+                            structure.structureType === STRUCTURE_SPAWN ||
+                            structure.structureType === STRUCTURE_TOWER) &&
                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
                 }
             });
