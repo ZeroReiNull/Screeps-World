3dc65a8e9c57ff2b4ef8b3c74d2878f2d8adf33a
ZeroTsu <103555203+ZeroReiNull@users.noreply.github.com>
2025-10-12 20:02:37 -0400

    Fix emergency creep not working

--- role.emergencyCreep.js
+++ role.emergencyCreep.js
@@ -1,37 +1,23 @@
-const roleEmergencyCreep = {
+var roleEmergencyCreep = {
+
     run: function(creep) {
-
-        if (creep.memory.delivering && creep.store[FIND_SOURCES] === 0) {
-            creep.memory.delivering = false;
-            // creep.say('collect');
-        }
-        if (!creep.memory.delivering && creep.store.getFreeCapacity() === 0) {
-            creep.memory.delivering = true;
-            // creep.say('deliver');
-        }
-
-        if (creep.memory.delivering) {
-            const target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType === STRUCTURE_EXTENSION ||
-                            structure.structureType === STRUCTURE_SPAWN) &&
-                           structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                }
-            });
-
-            if (target) {
-                if (creep.transfer(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target, { visualizePathStyle: { stroke: '#ffffff' } });
-                }
+        if(creep.store.getFreeCapacity() > 0) {
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
-        } else {
-            const droppedEnergy = creep.pos.findClosestByPath(FIND_DROPPED_RESOURCES, {
-                filter: (d) => d.resourceType === RESOURCE_ENERGY
+        }
+        else {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_EXTENSION ||
+                        structure.structureType == STRUCTURE_SPAWN) &&
+                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                }
             });
-
-            if (droppedEnergy) {
-                if (creep.pickup(droppedEnergy) === ERR_NOT_IN_RANGE) {
-                    creep.moveTo(droppedEnergy, { visualizePathStyle: { stroke: '#ffaa00' } });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
                 }
             }
         }
