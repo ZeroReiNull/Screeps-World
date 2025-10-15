31376c2b7ad016f465830dcd6714bd1486d24991
ZeroTsu <103555203+ZeroReiNull@users.noreply.github.com>
2025-10-15 11:50:42 -0400

    Increace wall repairer number

--- utility.autoSpawn.js
+++ utility.autoSpawn.js
@@ -7,7 +7,7 @@ const upgraderNum = 1;
 const builderNum = 1;
 const minerNum = 2;
 const roadieNum = 0;
-const wallRampartRepairerNum = 1;
+const wallRampartRepairerNum = 3;
 
 // Creep body configurations
 const harvesterBody = [CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE, MOVE];
