6145f6fe501bc709f26b06850f569611d92ed205
ZeroTsu <103555203+ZeroReiNull@users.noreply.github.com>
2025-10-15 11:20:51 -0400

    Remove console.log for manageMemory

--- utility.manageMemory.js
+++ utility.manageMemory.js
@@ -3,7 +3,6 @@ const manageMemory = {
     run: function() {
         for (const name in Memory.creeps) {
             if (!Game.creeps[name]) {
-                console.log('Clearing non-existing creep memory:', name);
                 delete Memory.creeps[name];
                 continue;
             }
@@ -11,7 +10,6 @@ const manageMemory = {
             const creep = Game.creeps[name];
             if (creep.memory.role == 'harvester' && creep.memory.harvestingTarget) {
                 if (!Game.getObjectById(creep.memory.harvestingTarget)) {
-                    console.log('Clearing invalid harvesting target for creep:', name);
                     delete creep.memory.harvestingTarget;
                 }
             }
