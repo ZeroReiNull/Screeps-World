b47bc30468acb4e3da3deb26ca803b29129696fe
ZeroTsu <103555203+ZeroReiNull@users.noreply.github.com>
2025-10-15 11:11:03 -0400

    Fix clean harvesting target if target not exists

--- cleanMemories.js
+++ cleanMemories.js
@@ -1,19 +1,23 @@
 const cleanMemories = {
-    
-    run: function() {
 
-        for(const name in Memory.creeps) {
-            if(!Game.creeps[name]) {
-                delete Memory.creeps[name];
+    run: function() {
+        for (const name in Memory.creeps) {
+            if (!Game.creeps[name]) {
                 console.log('Clearing non-existing creep memory:', name);
+                delete Memory.creeps[name];
+                continue;
             }
 
-            if (Game.creeps[name].role == 'harvester' && !Game.creeps[name].getObjectById(Game.creeps[name].memory.harvestingTarget)) {
-                delete Memory.creeps[name].harvestingTarget;
-                console.log('Clearing invalid harvesting target for creep:', name);
+            const creep = Game.creeps[name];
+            if (creep.memory.role == 'harvester' && creep.memory.harvestingTarget) {
+                if (!Game.getObjectById(creep.memory.harvestingTarget)) {
+                    console.log('Clearing invalid harvesting target for creep:', name);
+                    delete creep.memory.harvestingTarget;
+                }
             }
         }
     }
 };
 
 module.exports = cleanMemories;
+
