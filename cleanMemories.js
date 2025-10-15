acf31bb9d4cd9d0640cb4f6ef27088f6ae01ad47
ZeroTsu <103555203+ZeroReiNull@users.noreply.github.com>
2025-10-14 21:51:16 -0400

    bug

--- cleanMemories.js
+++ cleanMemories.js
@@ -7,6 +7,10 @@ const cleanMemories = {
                 delete Memory.creeps[name];
                 console.log('Clearing non-existing creep memory:', name);
             }
+
+            if (Memory.creeps[name].harvestingTargetId && !Game.getObjectById(Memory.creeps[name].harvestingTargetId)) {
+                delete Memory.creeps[name].harvestingTargetId;
+            }
         }
     }
 };
